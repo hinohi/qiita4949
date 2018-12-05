1ef062d249ff3b12d18fba163d7f73d11aabb2a3
Daiju Nakayama <42.daiju@gmail.com>
2018-12-05 22:58:55 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::io::stdin;
+
+fn is_prime(n: u128) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    for i in 2..n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_4949(mut n: u128) -> bool {
+    loop {
+        if n == 0 {
+            return false;
+        }
+        match n % 10 {
+            4 | 9 => return true,
+            _ => n /= 10,
+        }
+    }
+}
+
+fn solve_4949(n: usize) -> Vec<u128> {
+    let mut ans = Vec::new();
+    let mut i = 1;
+    while ans.len() < n {
+        if is_prime(i) && is_4949(i) {
+            ans.push(i);
+        }
+        i += 1;
+    }
+    ans
+}
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    let n = buf.trim().parse().expect("fail parse as integer");
+    let ans = solve_4949(n);
+    println!(
+        "{}",
+        ans.iter()
+            .map(|n| n.to_string())
+            .collect::<Vec<String>>()
+            .join(","),
+    );
+}
+
+#[test]
+fn test_prime() {
+    assert_eq!(is_prime(0), false);
+    assert_eq!(is_prime(1), false);
+    assert_eq!(is_prime(2), true);
+    assert_eq!(is_prime(57), false);
+    assert_eq!(is_prime(97), true);
+}
+
+#[test]
+fn test_4949() {
+    assert_eq!(is_4949(0), false);
+    assert_eq!(is_4949(1), false);
+    assert_eq!(is_4949(4), true);
+    assert_eq!(is_4949(9), true);
+    assert_eq!(is_4949(87318112), false);
+    assert_eq!(is_4949(987318112), true);
+}
